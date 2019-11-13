6873d03488308b21d750c29c164862440c46f636
AB <admin@rand.local>
2019-11-13 10:08:24 +0200

    disabling pychopg2

--- flaskqa/settings.py
+++ flaskqa/settings.py
@@ -1,7 +1,5 @@
 import os 
-import psycopg2
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
