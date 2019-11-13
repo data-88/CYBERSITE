d36d5f58c868d3fc2b9f276a325c98e8502b7c6d
AB <admin@rand.local>
2019-11-13 06:18:18 +0200

    database url issues

--- flaskqa/settings.py
+++ flaskqa/settings.py
@@ -1,5 +1,6 @@
 import os 
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+# SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = "postgres://gcklzkkygxoeeq:8b31f0330761c818ad90bac7a519784a84ecb20895a67836708e3b95426c9d86@ec2-54-75-249-16.eu-west-1.compute.amazonaws.com:5432/d9ptt6kvfesls0"
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
