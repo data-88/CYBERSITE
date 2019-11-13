c936393647b1a681c73acf4969fae030985bb412
AB <admin@rand.local>
2019-11-13 09:38:09 +0200

    postgres in settings.py

--- flaskqa/settings.py
+++ flaskqa/settings.py
@@ -1,5 +1,7 @@
 import os 
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = postgres://gcklzkkygxoeeq:8b31f0330761c818ad90bac7a519784a84ecb20895a67836708e3b95426c9d86@ec2-54-75-249-16.eu-west-1.compute.amazonaws.com:5432/d9ptt6kvfesls0
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 213asasdasd123fas3fasdasd241sadasdwqeravsgdadag
 SQLALCHEMY_TRACK_MODIFICATIONS = False
