9f8a13ff871ad02db88f777eb14842f55a02b38e
AB <admin@rand.local>
2019-11-13 10:00:37 +0200

    types in database url

--- flaskqa/settings.py
+++ flaskqa/settings.py
@@ -4,4 +4,4 @@ import psycopg2
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-conn = psycopg2.connect('DATABASE_URL', sslmode='require')
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
