d01f58d287e29af98972d9ec26f3e7736703c2f1
AB <admin@rand.local>
2019-11-13 09:58:55 +0200

    database and pychopg2

--- flaskqa/settings.py
+++ flaskqa/settings.py
@@ -1,5 +1,7 @@
 import os 
+import psycopg2
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+conn = psycopg2.connect('DATABASE_URL', sslmode='require')
