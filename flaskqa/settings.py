a245623d5d933730b06d8fa075b07f99e8533f35
AB <admin@rand.local>
2019-11-13 09:33:54 +0200

    postgres involved

--- flaskqa/settings.py
+++ flaskqa/settings.py
@@ -1,7 +1,5 @@
-import os
-import psycopg2
+import os 
 
-# SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-SQLALCHEMY_DATABASE_URL = "postgres://gcklzkkygxoeeq:8b31f0330761c818ad90bac7a519784a84ecb20895a67836708e3b95426c9d86@ec2-54-75-249-16.eu-west-1.compute.amazonaws.com:5432/d9ptt6kvfesls0"
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
